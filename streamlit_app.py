db48edc0205db598986de4edb0f385687ceb6b30
eglennsnowflake <124715217+eglennsnowflake@users.noreply.github.com>
2023-02-22 17:01:18 -0500

    Update streamlit_app.py
    Added breakpoint for debugging

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,6 +50,9 @@ except URLError as e:
 
 
 streamlit.header("The fruit load list contains:")
+#add breakpoint
+streamlit.stop
+
 #Snowflake-related functions
 def get_fruit_load_list:
      with my_cnx.cursor() as my_cur:
