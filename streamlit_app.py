14f02221ab9dd8c4e3eb2f35f40ba40d5c2184d7
eglennsnowflake <124715217+eglennsnowflake@users.noreply.github.com>
2023-02-22 16:38:48 -0500

    added try except and if else statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,25 +28,23 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 # new section to display fruityvice api response
-
 streamlit.header("Fruityvice Fruit Advice!")
-
-#Add an input box to screen and allow user to choose what fruit we look up via the api call
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-#import_requests
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#streamlit.text(fruityvice_response.json())  # just writes the data to the screen
-
-# Takes the json response and normalize it using pandas 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# Load data into datafram?
-streamlit.dataframe(fruityvice_normalized)
+try:
+ 
+  #Add an input box to screen and allow user to choose what fruit we look up via the api call
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  
+  if not fruit_choice:
+      streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)  
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error()
 
 #dont run anything past here while we troubleshoot
-streamlit.stop()
+#streamlit.stop()
 
 
 #import snowflake.connector
